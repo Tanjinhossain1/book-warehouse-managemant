19df028be29731343c265b6b8feb8c840e407bbc
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-05-03 16:22:49 +0600

    add new item input feuild update

--- src/Pages/AddNewItem/AddNewItem.js
+++ src/Pages/AddNewItem/AddNewItem.js
@@ -3,19 +3,33 @@ import { useForm } from 'react-hook-form';
 
 const AddNewItem = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+    const onSubmit = data => console.log(data);
+    //   name, price, img, description, quantity, supplierName,
+
+    return (
+        <form className='flex flex-col w-1/4 mx-auto mt-6' onSubmit={handleSubmit(onSubmit)}>
+            <input className='mb-2 border p-1 rounded-lg' placeholder='Name' {...register("name", { required: true })} />
+            {errors.name && <small className='text-red-600'>This field is required</small>}
+
+            <input className='mb-2 border p-1 rounded-lg' placeholder='Price' {...register("price", { required: true })} />
+            {errors.price && <small className='text-red-600'>This field is required</small>}
+
+            <input className='mb-2 border p-1 rounded-lg' placeholder='Quantity' {...register("quantity", { required: true })} />
+            {errors.quantity && <span className='text-red-600'>This field is required</span>}
+            
+            <input className='mb-2 border p-1 rounded-lg' placeholder='SupplierName' {...register("supplierName", { required: true })} />
+            {errors.supplierName && <span className='text-red-600'>This field is required</span>}
+
+            <input className='mb-2 border p-1 rounded-lg' placeholder='Image' {...register("img", { required: true })} />
+            {errors.img && <span className='text-red-600'>This field is required</span>}
 
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input defaultValue="test" {...register("example")} />
-      
-      <input {...register("exampleRequired", { required: true })} />
-      {errors.exampleRequired && <span>This field is required</span>}
-      
-      <input type="submit" />
-    </form>
-  );
+            <textarea className='mb-2 border p-1 rounded-lg' placeholder='Description' {...register("description", { required: true })} />
+            {errors.description && <span className='text-red-600'>This field is required</span>}
+ 
+            <input type="submit" className='mb-2 border rounded-lg bg-blue-600 text-white font-semibold text-xl py-2 ' value='Add Item' />
+        </form>
+    );
 };
 
 export default AddNewItem;
