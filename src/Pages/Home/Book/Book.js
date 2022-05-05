6482ef8cc79f006a884515e3105f15ce03afd3ff
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-05-05 17:42:21 +0600

    login page responsive done

--- src/Pages/Home/Book/Book.js
+++ src/Pages/Home/Book/Book.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Book = ({ book,children }) => {
     const { name, price, img, description, quantity, supplierName,_id } = book;
     return (
-        <div className=' mx-auto  mb-6 hover:bottom-10  relative border rounded-lg  shadow-lg hover:shadow-2xl w-3/4 sm:w-2/4 md:w-3/4 lg:w-4/4 xl:w-4/4 flex justify-center'>
+        <div className=' mx-auto  mb-6  relative border rounded-lg  shadow-lg hover:shadow-2xl w-3/4 sm:w-2/4 md:w-3/4 lg:w-4/4 xl:w-4/4 flex justify-center'>
            <div>
            <div className='flex justify-center'>
                 <img className='w-[100%] h-[450px] rounded-t-lg hover:shadow-2xl' src={img} alt="" />
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -65,7 +65,7 @@ const Login = () => {
     }
     return (
         <div className=''>
-            <div className='lg:w-1/4  md:w-3/4 w-3/4 sm:w-3/4 mx-auto border-2 border-purple-800 rounded-lg p-4 my-5'>
+            <div className='lg:w-2/4 xl:w-2/4 2xl:w-1/4  md:w-3/4 w-3/4 sm:w-3/4 mx-auto border-2 border-purple-800 rounded-lg p-4 my-5'>
                 <div>
                     <h1 className='text-2xl mb-4 font-semibold'>Login</h1>
 
@@ -75,7 +75,7 @@ const Login = () => {
                     <div className='flex flex-col mt-2 relative'>
                         <input className='border  px-1 py-1 w-4/4 ' type={`${open ? 'text' : 'password'}`} name="password" placeholder={`Password `} id="" required />
 
-                        {open ? <FontAwesomeIcon onClick={() => setOpen(!open)} className='absolute lg:ml-[316px] mt-2 ' icon={faEye} /> : <FontAwesomeIcon onClick={() => setOpen(!open)} className='absolute lg:ml-[316px] eyeSmall ml-[365px] sm:ml-[400px] mt-2 ' icon={faEyeSlash} />}
+                        {open ? <FontAwesomeIcon onClick={() => setOpen(!open)} className='absolute lg:ml-[316px] mt-2 eyeSmall' icon={faEye} /> : <FontAwesomeIcon onClick={() => setOpen(!open)} className='absolute lg:ml-[316px] eyeSmall ml-[365px] sm:ml-[400px] mt-2 ' icon={faEyeSlash} />}
                     </div>
 
                     <button onClick={resetPassword} className='flex justify-end text-purple-700 font-semibold mb-4'>Forgot Password?</button>
@@ -91,7 +91,9 @@ const Login = () => {
                     <p className='ml-11'>or</p>
                     <hr />
                 </div>
-                <button onClick={() => signInWithGoogle()} className=''><img className='shadow-2xl rounded-full mt-4 hover:bg-black' src="https://i.ibb.co/x8NkjXG/images-1-removebg-preview.png" alt="" /></button>
+               <div className='flex justify-center'>
+               <button onClick={() => signInWithGoogle()} className=''><img className='shadow-2xl rounded-full mt-4 hover:bg-black' src="https://i.ibb.co/x8NkjXG/images-1-removebg-preview.png" alt="" /></button>
+               </div>
             </div>
         </div>
     );
